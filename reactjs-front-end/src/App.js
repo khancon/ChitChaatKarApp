f253f794d933fe81665968f2b6c8d5361327267c
Ahnaf Khan <31942422+khancon@users.noreply.github.com>
2019-08-17 15:34:53 -0400

    React-JS

--- reactjs-front-end/src/App.js
+++ reactjs-front-end/src/App.js
@@ -26,6 +26,9 @@ const theme = createMuiTheme({
       contrastText: '#000',
     },
   },
+  typography: {
+    useNextVariants: true
+  }
 });
 function App() {
   return (
--- reactjs-front-end/src/pages/home.js
+++ reactjs-front-end/src/pages/home.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
 class home extends Component {
+    state = {
+        chaats: null
+    }
     componentDidMount(){
-
+        axios.get('/chaats')
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    chaats: res.data
+                })
+            })
+            .catch(err => console.log(err));
     } //4:51:00
     render() {
+        let recentChaatsMarkup = this.state.chaats ? (
+            this.state.chaats.map(chaat => <p>{chaat.body}</p>)
+        ) : <p>Loading...</p>
         return (
             <Grid container spacing={16}>
                 <Grid item sm={8} xs={12}>
-                    <p>Content...</p>
+                    {recentChaatsMarkup}
                 </Grid>
                 <Grid item sm={4} xs={12}>
                     <p>Profile...</p>
